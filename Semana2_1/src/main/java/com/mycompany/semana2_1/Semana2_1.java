93e63dc8a48b4706a330d7a35c2bf3e97dd174c1
Bernal <bernaljosecsfn3@gmail.com>
2024-08-11 23:49:52 -0500

    PrimerComit

---
+++ Semana2_1/src/main/java/com/mycompany/semana2_1/Semana2_1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana2_1;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author BernalXFK
+ */
+public class Semana2_1 {
+
+    public static void main(String[] args) {
+       JFrame.setDefaultLookAndFeelDecorated(true);
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        Horas_Minutos_Segundos prueba = new Horas_Minutos_Segundos();
+        prueba.setVisible(true);
+    }
+}
